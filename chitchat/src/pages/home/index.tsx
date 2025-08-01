0e1420dd7ad5eab81998281e7f609850249dcd24
XXUZZWZ <3189315662@qq.com>
2025-08-01 16:24:39 +0800

    feat(home): 添加搜索功能并优化样式
    - 在首页引入 Search 组件
    - 添加搜索功能，点击搜索框跳转到搜索页面
    - 在 App.css 中添加背景色透明和继承的样式类
    - 使用 bg-inherit 类优化搜索框背景色

--- chitchat/src/pages/home/index.tsx
+++ chitchat/src/pages/home/index.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from 'react-router-dom'
 import useTitle from '../../hooks/useTitle'
 import styles from './index.module.css'
+import { Search } from 'react-vant'
 const Home = () => {
   useTitle('首页')
-  return <div>Home</div>
+  const navigate = useNavigate();
+  const handleSearch = () => {
+    navigate('/search')
+  }
+  return (
+    <div>
+      <Search placeholder="请输入内容" onClickInput={handleSearch} className='bg-inherit'/>
+    </div>
+  )
 }
 
 export default Home
