694164ae54f0d1f4567b8de9952a2cf6b67e30cd
XXUZZWZ <3189315662@qq.com>
2025-08-05 14:32:22 +0800

    feat(mock): 添加用户登录、注册和检查登录状态的模拟接口
    - 新增 /api/login 接口用于用户登录
    - 新增 /api/user 接口用于检查用户登录状态
    - 新增 /api/user/register 接口用于用户注册
    - 引入 jsonwebtoken 包用于生成和验证 token

--- chitchat/mock/data.js
+++ chitchat/mock/data.js
@@ -1,4 +1,5 @@
 import Mock from "mockjs";
+import pkg from "jsonwebtoken";
 // 每页10个
 const getImages = (page, pageSize = 10) => {
   return Array.from({ length: pageSize }, (_, i) => ({
@@ -7,6 +8,10 @@ const getImages = (page, pageSize = 10) => {
     url: Mock.Random.image("300x400", Mock.Random.color(), "#fff", "image"),
   }));
 };
+
+const { sign, decode } = pkg;
+// 服务端私钥
+const secret = "jwt-demo-secret";
 const getAiRole = (page, pageSize = 10) => {
   return Array.from({ length: pageSize }, (_, i) => ({
     id: `${page}-${i}`,
@@ -147,5 +152,88 @@ export default [
         data: getAiRole(query.page),
       };
     },
+  },
+  // 登录
+  {
+    url: "/api/login",
+    method: "post",
+    response: (req) => {
+      const { username, password } = req.body;
+      if (username !== "admin" || password !== "123456") {
+        return {
+          code: 1,
+          message: "用户名或密码错误",
+        };
+      }
+      const token = sign(
+        {
+          user: {
+            id: "001",
+            username: "admin",
+          },
+        },
+        secret,
+        { expiresIn: "86400" }
+      );
+      return {
+        token,
+        data: {
+          id: "001",
+          username: "admin",
+        },
+      };
+    },
+  },
+  // 检查登录状态
+  {
+    url: "/api/user",
+    method: "GET",
+    response: (req) => {
+      const authHeader = req.headers["authorization"].split(" ")[1];
+      //console.log(authHeader);
+      const token = authHeader;
+      //console.log(token);
+      if (!token) {
+        return { code: 1, message: "未找到 token" };
+      }
+
+      try {
+        // const decoded = `fsfsdsfd`
+        // console.log(token);
+        // const tokenn =
+        //  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiMDAxIiwidXNlcm5hbWUiOiJhZG1pbiJ9LCJpYXQiOjE3NTMyNDQwMjYsImV4cCI6MTc1MzI0NDExMn0.WVEf6RkWnYHvaJL6T7Me1qFB2HXskN5hh4Gq38sxhU0";
+
+        console.log(token);
+        const data = decode(token);
+        // console.log(jwt.decode);
+        console.log(data, "AAA");
+
+        return {
+          code: 0,
+          message: "获取用户信息成功",
+          username: data.user.username,
+        };
+      } catch (err) {
+        return { code: 1, message: "token验证失败", err };
+      }
+    },
+  },
+  // 注册
+  {
+     url: "/api/user/register",
+     method: "post",
+     response: (req) => { 
+      const { username, password } = req.body;
+      if (username !== "admin" || password !== "123456") {
+        return {
+          code: 1,
+          message: "用户名或密码错误",
+        };
+      }
+      return {
+        code: 0,
+        message: "注册成功",
+      };
+     },
   },
 ];
